af404bdb95ff05a832878a10bf7c901d4398005f
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2020-12-25 10:45:44 -0600

    debian bare bones script

---
+++ debian/debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install vim links2 lxterminal xinit xserver-xorg-core xterm obconf openbox-menu openbox obmenu lightdm 
+apt-get install iceweasel pulseaudio pavucontrol
+apt-get install iptables-persistent tcpd
+apt-get install adwaita-icon-theme gnome-icon-theme hicolor-icon-theme papirus-icon-theme oxygen-icon-theme breeze-icon-theme
+apt-get install xtrlock arandr suckless-tools udiskie nitrogen pcmanfm mousepad
+apt-get install zip unzip fontconfig
